22ea7cdc082b8e869280e2b8df80ce78d518d83f
joewarsito <44288540+joewarsito@users.noreply.github.com>
2018-11-06 15:44:50 +0700

    Create kategori_bilangan.js

---
+++ kategori_bilangan.js
@@ -0,0 +1,11 @@
+var bulat, cacah, negatif, nol, asli, ganjil, prima, komposit
+var input = ['asli']
+
+function genap(){
+    if(input%2==0){
+        console.log('genap')
+    } else { console.log('ganjil')}
+}
+
+
+genap(13)
